2b4e9cc9778e7925f17fcb2e43565869fb374ce4
Nonamerz <a.g.kolesnik@yandex.ru>
2020-05-19 20:16:21 +0300

    Initial Commit

---
+++ traffic Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  traffic Light
+//
+//  Created by MacBook Pro on 19.05.2020.
+//  Copyright © 2020 Artem K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
