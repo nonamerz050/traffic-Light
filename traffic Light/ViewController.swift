3416d873ffecf20566b9ecf931bcf7a265e57436
Nonamerz <a.g.kolesnik@yandex.ru>
2020-05-19 20:31:43 +0300

    before code

--- traffic Light/ViewController.swift
+++ traffic Light/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func nextButtonPressed() {
+    }
+    
+    
 }
 
